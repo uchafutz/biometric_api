1c188d8fc44757fe5744d2e71c087c037a2605e8
uchafutz <uchafutz@gmail.com>
2023-11-20 16:11:06 +0300

    updates---on fetch data from the serve

--- index.js
+++ index.js
@@ -6,21 +6,80 @@ const authToken = '334c26b3264b5c741ef9bf5e3f20bad6';
 dotenv.config();
 const client = require('twilio')(accountSid, authToken);
 const app = express();
+const http = require('http');
 app.use(express.json());
+const currentTime = new Date();
+
+
+const year = currentTime.getFullYear();
+const month = currentTime.getMonth() + 1; // Months are zero-based, so add 1
+const day = currentTime.getDate();
+const hours = currentTime.getHours();
+const minutes = currentTime.getMinutes();
+const seconds = currentTime.getSeconds();
+
 
 
 const fetchData = async () => {
     try {
-        const response = await axios.get(process.env.BIO_URL);
-        return response.data;
+        const url = new URL(process.env.BIO_URL);
+        const options = {
+            hostname: url.hostname,
+            port: url.port || 8000,
+            path: url.pathname,
+            method: 'GET',
+        };
+
+        return new Promise((resolve, reject) => {
+            const req = http.request(options, (res) => {
+                let data = '';
+
+                // A chunk of data has been received.
+                res.on('data', (chunk) => {
+                    data += chunk;
+                });
+
+                // The whole response has been received.
+                res.on('end', () => {
+                    console.log('Data successfully obtained', `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`);
+                    resolve(data);
+                });
+            });
+
+            // Handle errors during the HTTP request.
+            req.on('error', (error) => {
+                console.error('Failed to fetch data:', error.message);
+                reject(error);
+            });
+
+            // End the request (this is important for GET requests).
+            req.end();
+        });
     } catch (error) {
-        console.error('Failed to fetch data:', error.message);
+        console.error('Failed to fetch data:', `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`, error.message);
         throw error;
     }
-}
+};
+
+// Example usage:
+fetchData()
+    .then((data) => {
+        // Process the data here
+        console.log(data);
+    })
+    .catch((error) => {
+        // Handle errors here
+        console.error(error);
+    });
 
 const filterData = (data) => {
-    return data.filter(item => { item.name });
+    if (Array.isArray(data)) {
+        return data.filter(item => { /* your filter condition */ });
+    } else {
+        console.error('Input is not an array.');
+        // Handle the error or return an appropriate value
+        return [];
+    }
 };
 
 const sendMessage = async (filteredData) => {
@@ -38,17 +97,17 @@ const sendMessage = async (filteredData) => {
         return false; // Indicate failure
     }
 };
+
 const processDataAndSendMessage = async () => {
     try {
         const data = await fetchData();
 
         if (data && data.length > 0) {
-            const filteredData = filterData(data);
-            const success = await sendMessage(filteredData);
+            //const filteredData = filterData(data);
+            const success = await sendMessage(data);
 
             if (!success) {
-                // Retry after a set time interval (e.g., 5 minutes)
-                setTimeout(processDataAndSendMessage, 2 * 60 * 1000);
+                setTimeout(processDataAndSendMessage, 1 * 60 * 1000);
             }
         } else {
 
@@ -56,8 +115,8 @@ const processDataAndSendMessage = async () => {
         }
     } catch (error) {
         console.error('Error in processDataAndSendMessage:', error.message);
-        // Retry after a set time interval (e.g., 5 minutes)
-        setTimeout(processDataAndSendMessage, 2 * 60 * 1000);
+
+        setTimeout(processDataAndSendMessage, 1 * 60 * 1000);
     }
 };
 //test bio
